ca73361fc86ff2f5f905176495a86219070974ed
Anibal R. João <anibal.Joao@unizambeze.ac.mz>
2026-03-04 18:44:01 +0200

    obsfuscating the password fild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation)
+    implementation(libs.androidx.material.icons)
 
     //Test
     testImplementation(libs.junit)
--- app/src/main/java/com/anibal/kingburguer/compose/login/LoginScreen.kt
+++ app/src/main/java/com/anibal/kingburguer/compose/login/LoginScreen.kt
@@ -10,19 +10,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -38,6 +50,7 @@ fun LoginScreen(navController: NavHostController) {
     ) { innerPadding ->
 
         val scrollState = rememberScrollState()
+        var passwordHidden by remember { mutableStateOf(true) }
 
         Column(
             modifier = Modifier
@@ -64,7 +77,31 @@ fun LoginScreen(navController: NavHostController) {
                 placeholder = R.string.hint_password,
                 keyboardType = KeyboardType.Password,
                 imeAction = ImeAction.Done,
-                modifier = Modifier.padding(horizontal = 20.dp)
+                offuscate = passwordHidden,
+                modifier = Modifier.padding(horizontal = 20.dp),
+                trailingIcon = {
+                    IconButton(
+                        onClick = {
+                            passwordHidden = !passwordHidden
+                        }
+                    ) {
+                        val icon = if (passwordHidden){
+                            Icons.Filled.VisibilityOff
+                        }else{
+                            Icons.Filled.Visibility
+                        }
+
+                        val description = if (passwordHidden){
+                            stringResource(R.string.show_password)
+                        }else{
+                            stringResource(R.string.hide_password)
+                        }
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = description
+                        )
+                    }
+                }
             ){
 
             }
@@ -93,12 +130,15 @@ fun KingTextField(
     @StringRes placeholder: Int,
     keyboardType: KeyboardType = KeyboardType.Text,
     imeAction: ImeAction = ImeAction.Next,
+    offuscate: Boolean = false,
     modifier: Modifier = Modifier,
+    trailingIcon: @Composable (() -> Unit)? = null,
     onValueChange: (String) -> Unit
 ){
     OutlinedTextField(
         modifier = modifier.fillMaxWidth(),
         value = value,
+        maxLines = 1,
         onValueChange = onValueChange,
         label = {
             Text(
@@ -113,7 +153,12 @@ fun KingTextField(
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType,
             imeAction = imeAction
-        )
+        ),
+        trailingIcon = trailingIcon,
+        visualTransformation = if (offuscate)
+            PasswordVisualTransformation()
+        else
+            VisualTransformation.None
     )
 }
 
