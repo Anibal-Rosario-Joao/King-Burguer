445cc276836125f35d05f289c7293e3d234f0f53
Anibal R. João <anibal.Joao@unizambeze.ac.mz>
2026-03-17 13:17:14 +0200

    updating the screen

--- app/src/main/java/com/anibal/kingburguer/compose/signup/SignUpScreen.kt
+++ app/src/main/java/com/anibal/kingburguer/compose/signup/SignUpScreen.kt
@@ -1,2 +1,264 @@
 package com.anibal.kingburguer.compose.signup
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.anibal.kingburguer.R
+import com.anibal.kingburguer.component.KingAlert
+import com.anibal.kingburguer.component.KingButton
+import com.anibal.kingburguer.component.KingTextField
+import com.anibal.kingburguer.component.KingTextTitle
+import com.anibal.kingburguer.compose.Screen
+import com.anibal.kingburguer.ui.theme.KingBurguerTheme
+import com.anibal.kingburguer.viewmodels.SignUpViewModel
+
+
+@Composable
+fun SignUpScreen(
+    viewModel: SignUpViewModel = viewModel(),
+    navController:  NavHostController
+){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Scaffold(
+
+        ) { contentPadding ->
+
+            SignUpContentScreen(
+                viewModel = viewModel,
+                navController = navController,
+                modifier = Modifier.padding(top = contentPadding.calculateTopPadding())
+            )
+        }
+    }
+
+}
+
+@Composable
+fun SignUpContentScreen(
+    viewModel : SignUpViewModel ,
+    navController: NavHostController,
+    modifier: Modifier
+){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val scrollState = rememberScrollState()
+        var passwordHidden by remember { mutableStateOf(true) }
+
+        val uiState by viewModel.uiState.collectAsState()
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .verticalScroll(scrollState),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(14.dp, Alignment.Top)
+            ) {
+
+                uiState.error?.let {
+                    KingAlert(
+                        onDismissRequest = {
+                            //TODO
+                        },
+                        confirmationButton = {
+                            viewModel.reset()
+                        },
+                        dialogTitle = stringResource(R.string.app_name),
+                        dialogText = uiState.error!!,
+                        icon = Icons.Filled.Info
+                    )
+                }
+                Spacer(modifier = Modifier.padding(top = 24.dp))
+                KingTextTitle(
+                    text = stringResource(R.string.sign_up)
+                )
+                KingTextField(
+                    value = viewModel.name,
+                    label = R.string.name,
+                    placeholder = R.string.hint_name,
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ) {
+
+                }
+
+                KingTextField(
+                    value = viewModel.email,
+                    label = R.string.email,
+                    placeholder = R.string.hint_email,
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ) {
+
+                }
+
+                KingTextField(
+                    value = viewModel.password,
+                    label = R.string.password,
+                    placeholder = R.string.hint_password,
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next,
+                    offuscate = passwordHidden,
+                    trailingIcon = {
+                        IconButton(
+                            onClick = {
+                                passwordHidden = !passwordHidden
+                            }
+                        ) {
+                            val icon = if (passwordHidden) {
+                                Icons.Filled.VisibilityOff
+                            } else {
+                                Icons.Filled.Visibility
+                            }
+
+                            val description = if (passwordHidden) {
+                                stringResource(R.string.show_password)
+                            } else {
+                                stringResource(R.string.hide_password)
+                            }
+                            Icon(
+                                imageVector = icon,
+                                contentDescription = description
+                            )
+                        }
+                    }
+                ) {
+                    //Evento de onValueChange()
+                }
+
+                KingTextField(
+                    value = viewModel.confirmPassword,
+                    label = R.string.confirm_password,
+                    placeholder = R.string.hint_confirm_password,
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next,
+                    offuscate = passwordHidden,
+                    trailingIcon = {
+                        IconButton(
+                            onClick = {
+                                passwordHidden = !passwordHidden
+                            }
+                        ) {
+                            val icon = if (passwordHidden) {
+                                Icons.Filled.VisibilityOff
+                            } else {
+                                Icons.Filled.Visibility
+                            }
+
+                            val description = if (passwordHidden) {
+                                stringResource(R.string.show_password)
+                            } else {
+                                stringResource(R.string.hide_password)
+                            }
+                            Icon(
+                                imageVector = icon,
+                                contentDescription = description
+                            )
+                        }
+                    }
+                ) {
+                    //Evento de onValueChange()
+                }
+
+                KingTextField(
+                    value = viewModel.document,
+                    label = R.string.document,
+                    placeholder = R.string.hint_document,
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ) {
+
+                }
+
+                KingTextField(
+                    value = viewModel.birthDay,
+                    label = R.string.birthday,
+                    placeholder = R.string.hint_birthday,
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ) {
+
+                }
+
+                KingButton(
+                    text = stringResource(R.string.send),
+
+                    ) {
+                    //Evento de onClick()
+                    viewModel.send()
+                }
+            }
+
+            Image(
+                modifier = Modifier
+                    .fillMaxSize(),
+                alignment = Alignment.BottomCenter,
+                painter = painterResource(R.drawable.cover3),
+                contentDescription = stringResource(R.string.burguer)
+            )
+
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SignUpnScreenLightPreview() {
+    KingBurguerTheme (dynamicColor = false){
+        SignUpScreen(navController = rememberNavController())
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SignUpScreenDarkPreview() {
+    KingBurguerTheme (dynamicColor = false, darkTheme = true){
+        SignUpScreen(navController = rememberNavController())
+    }
+}
